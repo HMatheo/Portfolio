dec637970681d90e005016c7dbfdaff63bb3c17c
Mathéo Hersan <matheohersan@MacBook-Pro-de-Matheo.local>
2024-02-01 21:29:27 +0100

    NavBar almost finished
    I almost finished my navbar with responsive. Also added scss.

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+/* ----- NAVIGATION BAR FUNCTION ----- */
+function menuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+        menuBtn.className += " responsive";
+    } else {
+        menuBtn.className = "nav-menu";
+    }
+}
+
+//shadow on navbar
+window.onscroll = function (){headerShadow()};
+
+function headerShadow(){
+    const navHeader = document.getElementById("header");
+
+    if(document.body.scrollTop > 50  || document.documentElement.scrollTop > 50){
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+    } else{
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+    }
+}
